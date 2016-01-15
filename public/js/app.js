52746059619bc1eeb52ac19a5988730f9697e69c
Miguel Crespo <miguel.crespo6@gmail.com>
2016-01-14 20:30:15 -0500

    Creando el modal de configuracion inicial del cubo
    Creando e implementando el modal que se encargara de preguntar al usuario sobre la informacion referente al cubo (t, n, m)

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,32 @@
 /**
  * Created by Miguel on 1/14/16.
  */
+
+$('#myModal').modal('show');
+
+var vm = new Vue({
+    el: '#main',
+    data: {
+        cube: {
+            t: '',
+            n: '',
+            m: ''
+        },
+        comandos: []
+    },
+    // define methods under the `methods` object
+    methods: {
+        config: function (e) {
+            e.preventDefault();
+
+            if (!vm.cube.tests || !vm.cube.matrix || !vm.cube.m) {
+                return alert("Por favor completa el formulario!");
+            }
+
+
+            $('#myModal').modal('hide');
+
+
+        }
+    }
+});
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,5 +3,37 @@
 @section('content')
     Pantalla principal
 
+
+    <div class="modal fade" tabindex="-1" role="dialog" id="myModal" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="" novalidate>
+                    <div class="modal-header">
+                        <h4 class="modal-title">Configura el cubo</h4>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Tests</label>
+                            <input type="number" v-model="cube.tests" class="form-control" id="exampleInputEmail1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">N Matrix</label>
+                            <input type="number" v-model="cube.matrix" class="form-control" id="exampleInputEmail1" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">N Comandos</label>
+                            <input type="number" v-model="cube.m" class="form-control" id="exampleInputEmail1" placeholder="">
+                        </div>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" v-on:click="config" class="btn btn-primary">Configurar</button>
+                    </div>
+                </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 @endsection
 
