85bf67e3ff6d427308e72bc43eb95665045c1a26
Miguel Crespo <miguel.crespo6@gmail.com>
2016-01-14 20:14:44 -0500

    Creando el frontend de la vista principal
    Se creo el layout de la vista principal, que se encargara de guiar al usuario a digitar e interactuar con el servidor.

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Miguel on 1/14/16.
+ */
---
+++ resources/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cube Summation challenge</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="container" id="main">
+    @yield('content')
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.14/vue.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
